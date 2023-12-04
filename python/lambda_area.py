45dc4008348f9a572d189bd6dc9ccf92e40f68cf
suneeb <ssuneebvishnu@gmail.com>
2023-12-04 13:47:44 +0530

    areas using lambda

---
+++ python/lambda_area.py
@@ -0,0 +1,10 @@
+# x =int(input("enter the side of square"))
+
+sq_area = lambda a : a*a
+print(sq_area(5))
+
+rect_area = lambda l,b : l*b
+print(rect_area(2,3))
+
+tri_area = lambda b,h : 0.5 * b * h
+print(tri_area(3,2))
